6b287e0a4560f53d11510c5d1f22fbcd8942c80f
user-PC\user <8seeat8@gmail.com>
2017-01-05 12:20:04 +0900

    임시

--- src/main/java/com/spring/main/dao/ResultInterface.java
+++ src/main/java/com/spring/main/dao/ResultInterface.java
@@ -7,6 +7,12 @@ import com.spring.main.dto.EntirelyDto;
 public interface ResultInterface {
 
 	//경기일,t_idx 가져오기
-	ArrayList<EntirelyDto> matchDay();
+	ArrayList<EntirelyDto> matchDay(String Dday);
+
+	//difference 
+	void difference(String string, String idx);
+
+	//t_rankpoint
+	void rankpoint(int t_rankpoint, String result, String t_idx);
 
 }
--- src/main/java/com/spring/main/service/ResultService.java
+++ src/main/java/com/spring/main/service/ResultService.java
@@ -3,6 +3,7 @@ package com.spring.main.service;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
@@ -37,11 +38,44 @@ public class ResultService {
 		ModelAndView mav = new ModelAndView();
 		inter = sqlSession.getMapper(ResultInterface.class);
 		
-		Date date = new Date();
-		SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
-		String today = format.format(date);
-		ArrayList<EntirelyDto> Dday = inter.matchDay();
-		logger.info(today);
+		/*Date date = new Date();*/
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		GregorianCalendar cal = new GregorianCalendar();
+		cal.add(cal.DATE, -3);
+		Date date = cal.getTime();
+		String Dday = format.format(date);
+		ArrayList<EntirelyDto> ent = inter.matchDay(Dday);
+		
+		for(int i=0; i<ent.size(); i+=2){
+			int j=i+1;
+			String con1 = ent.get(i).getE_condition();
+			String con2 = ent.get(j).getE_condition();
+			String team = ent.get(i).getE_team();
+			String[] tname = team.split(":");
+			logger.info(tname[0]+"/"+tname[1]);
+			String idx = ent.get(i).getMch_idx();
+			if(con1.equals(con2)){
+				inter.difference("0:0",idx);/*
+				inter.rankpoint(2,"t_draw",tname[0]);
+				inter.rankpoint(2,"t_draw",tname[1]);*/
+			}else{
+				if(con1.equals("1")){
+					String dif = ent.get(i).getE_difference();
+					String[] score = dif.split(":");
+					inter.difference(dif,idx);
+					logger.info(score[0]+"/"+score[1]);
+					/*if(score[0]>score[1]){
+						inter.rankpoint(3,"t_draw",tname[0]);
+					}*/
+				}else{
+					String dif = ent.get(j).getE_difference();
+					String[] score = dif.split(":");
+					inter.difference(dif,idx);
+					logger.info(score[0]+"/"+score[1]);
+				}
+			}
+		}
+		logger.info(Dday);
 		return null;
 	}
 
